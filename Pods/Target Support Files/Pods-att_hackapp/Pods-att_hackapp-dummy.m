71e3cf7d04355a61be16c261a04d6ac9d2ff2da8
Luca Hagel <hagel.luca@gmail.com>
2016-07-15 19:11:06 -0700

    Create Xcode Project, install firebase, swiftyjson, alamofire

---
+++ Pods/Target Support Files/Pods-att_hackapp/Pods-att_hackapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_att_hackapp : NSObject
+@end
+@implementation PodsDummy_Pods_att_hackapp
+@end
---
+++ Pods/Target Support Files/Pods-att_hackapp/Pods-att_hackapp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_att_hackappVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_att_hackappVersionString[];
+
