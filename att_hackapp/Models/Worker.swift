b1e7fc53a6bf6e658c604c807362d4e7b4df5b6d
Luca Hagel <hagel.luca@gmail.com>
2016-07-15 20:10:12 -0700

    Add Worker Base Structure

--- att_hackapp/Models/Worker.swift
+++ att_hackapp/Models/Worker.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+enum WorkerType {
+    case Plumber
+    case Electrician
+    case Medical
+    case Misc
+}
+
+
+class Worker {
+    var phoneNumber: String!
+    var name: String!
+    var imgURL: String
+    var workerId: String!
+    var description: String!
+    var rating: Float
+    var type: WorkerType
+    
+    init(id: String) {
+        Alamofire.request(.GET, url).validate().responseJSON { response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    //TODO: IMPLEMENT                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
+    }
+}
