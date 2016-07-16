84461a3812a44dfe49da801833b364f7835dc91f
Luca Hagel <hagel.luca@gmail.com>
2016-07-15 19:31:10 -0700

    Add boilerplate files

---
+++ att_hackapp/Models/Worker.swift
@@ -0,0 +1,9 @@
+//
+//  Worker.swift
+//  att_hackapp
+//
+//  Created by Luca Hagel on 7/15/16.
+//  Copyright © 2016 Luca Hagel. All rights reserved.
+//
+
+import Foundation
---
+++ att_hackapp/Views/WorkerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkerTableViewCell.swift
+//  att_hackapp
+//
+//  Created by Luca Hagel on 7/15/16.
+//  Copyright © 2016 Luca Hagel. All rights reserved.
+//
+
+import UIKit
+
+class WorkerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
